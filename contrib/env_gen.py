c23c8a3e295ef84fdac03ef0ea76507e88dc1a17
Deyved Antonio <deyved.antonio@gmail.com>
2026-07-14 21:20:47 -0300

    feat: initialize Django project structure and configure environment-based settings

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -1,6 +1,7 @@
 """
 Python SECRET_KEY generator.
 """
+
 import random
 
 chars = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!?@#$%^&*()"
@@ -29,11 +30,14 @@ ALLOWED_HOSTS=127.0.0.1,.localhost,0.0.0.0
 #EMAIL_HOST_USER=
 #EMAIL_HOST_PASSWORD=
 #EMAIL_USE_TLS=True
-""".strip() % (secret_key, password)
+""".strip() % (
+    secret_key,
+    password,
+)
 
 # Writing our configuration file to '.env'
-with open('.env', 'w') as configfile:
+with open(".env", "w") as configfile:
     configfile.write(CONFIG_STRING)
 
-print('Success!')
-print('Type: cat .env')
+print("Success!")
+print("Type: cat .env")
