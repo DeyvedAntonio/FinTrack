60c0bc679e9b2a20ce659508e153443022563d1c
Deyved Antonio <deyved.antonio@gmail.com>
2026-07-19 14:41:23 -0300

    feat: deploy pythonanywhere

--- apps/settings.py
+++ apps/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'django_extensions',
@@ -48,11 +49,12 @@ REST_FRAMEWORK = {
     ],
 }
 
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
